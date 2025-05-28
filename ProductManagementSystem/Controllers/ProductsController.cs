a665a89f1ff3365f386c870fa99710f6417b0721
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-28 12:34:52 +0100

    refact(controller): Implement controller for retreving a product.

--- ProductManagementSystem/Controllers/ProductsController.cs
+++ ProductManagementSystem/Controllers/ProductsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductManagementSystem.Data;
+using ProductManagementSystem.DTOs;
+using ProductManagementSystem.Models;
 
 namespace ProductManagementSystem.Controllers
 {
@@ -8,5 +13,66 @@ namespace ProductManagementSystem.Controllers
     public class ProductsController : ControllerBase
     {
 
+        private readonly ProductDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductsController(ProductDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("GetProducts")]
+        public async Task<ActionResult<List<ProductDTO>>> GetProducts()
+        {
+            try
+            {
+                List<Product> products = await _context.Products
+                    .AsNoTracking()
+                    .Include(p => p.Category)
+                    .ToListAsync();
+
+                if (products.Count <= 0)
+                {
+                    return NotFound("Product list could not be located");
+                }
+
+                List<ProductDTO> productDTOs = _mapper.Map<List<ProductDTO>>(products);
+
+                return Ok(productDTOs);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+
+        }
+
+        [HttpGet("GetProductById/{Id}")]
+        public async Task<ActionResult<ProductDTO>> GetProductById([FromRoute] int Id)
+        {
+            try
+            {
+                var product = await _context.Products
+                    .AsNoTracking()
+                    .Include(p => p.Id)
+                    .FirstOrDefaultAsync(p => p.Id == Id);
+
+                if(product == null)
+                {
+                    return NotFound($"No product with {Id} found");
+                }
+
+                ProductDTO productDTO = _mapper.Map<ProductDTO>(product);
+
+                return Ok(productDTO);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error occurred: {ex.Message}");
+            }
+        }
     }
 }
