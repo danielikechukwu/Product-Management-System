e0ff93f81ac402a1122bcda5265fd2b1c08d2474
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-28 11:28:39 +0100

    feat(connection-setup): Set-up database connection and Program Automapper, Dependency Injection and Middleware.

--- ProductManagementSystem/Program.cs
+++ ProductManagementSystem/Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using ProductManagementSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Register AutoMapper and the mapping profile
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    // Optionally, configure JSON options or other formatter settings
+    .AddJsonOptions(options =>
+    {
+        // Configure JSON serializer settings to keep the Original names in serialization and deserialization
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    });
+
+// Register the ProductDbContext with dependency injection
+builder.Services.AddDbContext<ProductDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDBConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
